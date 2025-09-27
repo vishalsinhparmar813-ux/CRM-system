c169ff809c351c31f0a6ddc5dc513d0585e159b2
vishalsinh <vishalsinhparmar821@gamil.com>
2025-09-27 11:50:31 +0530

    updated endpoint in client

--- client/src/constant/common.js
+++ client/src/constant/common.js
@@ -1,5 +1,5 @@
 // // export const apiEndpoint = "https://techyroots.com:3498/api/"
-export const apiEndpoint = "http://localhost:3010/";
+export const apiEndpoint = import.meta.env.VITE_API_ENDPOINT ;
 // export const apiEndpoint = "http://192.168.1.15:8443/api/";
 // export const apiEndpoint = "https://api.hamsterkombatclone.com/api/"; // Danuki Dev
 // export const apiEndpoint = "https://dev-danuki-api.pwrlabs.io/api/"; //Danuki Dev New URL
